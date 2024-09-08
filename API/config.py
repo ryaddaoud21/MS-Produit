5804d09d413376452fc0a4667dc0472c1939735a
ryaddaoud21 <Chtaybo@22>
2024-09-08 02:43:33 +0200

    switch database

--- API/config.py
+++ API/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'mysql+pymysql://root@localhost/produit_db')
-    #SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'mysql+pymysql://root@mysql-db/produit_db')
+    #SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'mysql+pymysql://root@localhost/produit_db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'mysql+pymysql://root@mysql-db/produit_db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
