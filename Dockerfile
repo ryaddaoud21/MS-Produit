a3be464eacbc8a07aa35aa1f4a5ed948ad08581f
ryaddaoud21 <Chtaybo@22>
2024-09-15 15:52:42 +0200

    update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -12,7 +12,6 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 # Exposer le port sur lequel l'application Flask s'exécutera
 EXPOSE 5002 8000
-# Expose port 5002 for API and port 8000 for Prometheus metrics
 
-# Commande pour démarrer l'application Flask avec Waitress et le consommateur RabbitMQ
+# Commande pour démarrer l'application Flask
 CMD ["waitress-serve", "--port=5002", "produit_api:app"]
--- produit_api.py
+++ produit_api.py
@@ -17,9 +17,16 @@ db.init_app(app)
 app.register_blueprint(auth_blueprint, url_prefix='/auth')
 app.register_blueprint(products_blueprint, url_prefix='/products')
 
+@app.route('/')
+def index():
+    return jsonify({"message": "Produit service is running!"})
+
 if __name__ == '__main__':
-    start_rabbitmq_consumers(app)
+    # Lancer le consommateur RabbitMQ
+    threading.Thread(target=start_rabbitmq_consumers, args=(app,)).start()
 
     # Lancer le serveur HTTP pour Prometheus sur le port 8000
     start_http_server(8000)
+
+    # Lancer l'application Flask sur le port 5002
     app.run(host='0.0.0.0', port=5002)
